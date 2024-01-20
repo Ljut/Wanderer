b7cb7e54ff7ba4bcc259999603b80c4316c33081
Ljut <ljutikazlatan@gmail.com>
2024-01-20 17:14:06 +0100

    2024-1-20 Added Google map - jos ne radi

--- app/src/main/java/com/example/wanderer/MainActivity.java
+++ app/src/main/java/com/example/wanderer/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.wanderer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.room.Room;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 //import com.example.wanderer.jsondb.JsonDB;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,24 +25,37 @@ import org.json.JSONObject;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     static private boolean firstRun=false;
     TextView textView;
     Button button;
+    private GoogleMap gMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.autori);
-        button = findViewById(R.id.button);
+        //textView = findViewById(R.id.autori);
+        //button = findViewById(R.id.button);
         Context context = this;
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+
+        /*if(!checkLocationPermission()) {
+            return ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        }*/
+
         System.out.println("test");
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "Gradovi").build();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        /*button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GradDao gradDao = db.gradDao();
@@ -44,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-        });
+        });*/
         if(!firstRun) {
 
             /*JsonDB jsonDB = new JsonDB();
@@ -60,4 +81,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean checkLocationPermission() {
+        return true;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
 }
